a53c163d94f345290dfa4700806685e0a35582f1
GIRI TALARI <giritalari321@gmail.com>
2020-08-14 12:28:56 +0530

    views

--- browser/app1/views.py
+++ browser/app1/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'giri.html')
+
+
+def home(request):
+    return render(request, 'sai.html')
